733986c5cfd13c9b0b07bc6286ed1f322e76a860
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-18 21:50:57 -0400

    feat: routes init
    VEN-12

---
+++ src/components/Routes.tsx
@@ -0,0 +1,5 @@
+export const routes = [
+  { key: "home", focusedIcon: "home", unfocusedIcon: "home-outline" },
+  { key: "prayerRequest", focusedIcon: "hands-pray" },
+  { key: "give", focusedIcon: "hand-coin", unfocusedIcon: "hand-coin-outline" },
+];
