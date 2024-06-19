88ec94cdda813dd756696330377c70911f204140
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-18 21:50:45 -0400

    feat: add logo to header
    VEN-12

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,12 @@ import * as React from "react";
 import { Image } from "react-native";
 import { Appbar } from "react-native-paper";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-import logo from "../../assets/logo.svg";
 
 const Header = () => {
   return (
     <Appbar.Header style={{ marginHorizontal: 10 }} elevated={true}>
       <Image
-        source={require(logo)}
+        source={require("../../assets/logo.svg")}
         style={{ width: 30, height: 30, marginRight: "auto" }}
       />
       <Icon name="account-circle" size={30} color="#000" />
