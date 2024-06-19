fd36daee94b7309f709269c64656742f4c97a884
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-18 21:49:49 -0400

    feat: add bottomnav to app
    VEN-12

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider as PaperProvider } from "react-native-paper";
 import Header from "./src/components/Header";
+import BottomNav from "./src/components/BottomNavigation";
 
 export default function Main() {
   return (
     <SafeAreaProvider>
       <PaperProvider>
         <Header />
+        <BottomNav />
       </PaperProvider>
     </SafeAreaProvider>
   );
