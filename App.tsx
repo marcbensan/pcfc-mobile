fc006af91ecc7f010902ec15c26a13c31729c9ae
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-20 23:22:40 -0400

    feat: add theme
    VEN-12

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider as PaperProvider } from "react-native-paper";
 import Header from "./src/components/Header";
 import BottomNav from "./src/components/BottomNavigation";
+import { theme } from "./src/theme/Theme";
+import { View, Text } from "react-native";
 
 export default function Main() {
   return (
     <SafeAreaProvider>
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <Header />
         <BottomNav />
       </PaperProvider>
--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,24 +1,35 @@
 import * as React from "react";
-import { BottomNavigation } from "react-native-paper";
+import { BottomNavigation, useTheme } from "react-native-paper";
 import Home from "../pages/Home";
 import PrayerRequest from "../pages/PrayerRequest";
 import Give from "../pages/Give";
-import { routes } from "./Routes";
+import { routes } from "../routes/Routes";
 
 const BottomNav = () => {
   const [index, setIndex] = React.useState(0);
+  const theme = useTheme();
 
   const renderScene = BottomNavigation.SceneMap({
     home: Home,
-    prayerRequest: PrayerRequest,
+    prayers: PrayerRequest,
     give: Give,
   });
 
   return (
     <BottomNavigation
+      barStyle={{ backgroundColor: theme.colors.primary }}
       navigationState={{ index, routes }}
       onIndexChange={setIndex}
       renderScene={renderScene}
+      inactiveColor="#efeee9"
+      activeColor="white"
+      activeIndicatorStyle={{
+        backgroundColor: "#041327",
+        borderTopLeftRadius: 8,
+        borderTopRightRadius: 8,
+        borderBottomLeftRadius: 8,
+        borderBottomRightRadius: 8,
+      }}
     />
   );
 };
