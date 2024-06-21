dce550461b2e23b38a0fec26336be9461d425801
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-20 23:23:59 -0400

    feat: move routes
    VEN-12

--- src/components/Routes.tsx
+++
@@ -1,5 +0,0 @@
-export const routes = [
-  { key: "home", focusedIcon: "home", unfocusedIcon: "home-outline" },
-  { key: "prayerRequest", focusedIcon: "hands-pray" },
-  { key: "give", focusedIcon: "hand-coin", unfocusedIcon: "hand-coin-outline" },
-];
---
+++ src/routes/Routes.tsx
@@ -0,0 +1,16 @@
+export const routes = [
+  {
+    key: "home",
+    title: "Home",
+    focusedIcon: "home",
+    unfocusedIcon: "home-outline",
+    color: "white",
+  },
+  { key: "prayers", title: "Prayers", focusedIcon: "hands-pray" },
+  {
+    key: "give",
+    title: "Giving",
+    focusedIcon: "hand-coin",
+    unfocusedIcon: "hand-coin-outline",
+  },
+];
