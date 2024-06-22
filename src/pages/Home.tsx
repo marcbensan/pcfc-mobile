3a44cd13782037ccd55e4a3ab2f8bc4d0aa090b5
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-21 22:50:15 -0400

    fix: update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,27 @@
-import { Text } from 'react-native-paper'
-import CardComponent from '../components/Card'
+import { Text } from "react-native-paper";
+import CardComponent from "../components/Card";
+import { FlatList, View } from "react-native";
+import SubCardComponent from "../components/SubCard";
+import { youtubeData } from "../data/YoutubeData";
 
 const Home = () => {
   return (
-    <CardComponent/>
-  )
-}
+    <View style={{ padding: 15 }}>
+      <CardComponent />
+      <Text variant="titleLarge" style={{ marginTop: 35, marginBottom: 5 }}>
+        Recently Uploaded
+      </Text>
+      <FlatList
+        data={youtubeData}
+        renderItem={({ item }) => (
+          <SubCardComponent image={item.photo} title={item.title} />
+        )}
+        keyExtractor={(item) => item.id}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
 
-export default Home
+export default Home;
