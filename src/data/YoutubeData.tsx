b4facf825e122535dbaba3f56e1741b752f4c81b
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-21 22:50:06 -0400

    feat: add yt data

---
+++ src/data/YoutubeData.tsx
@@ -0,0 +1,17 @@
+export const youtubeData = [
+  {
+    id: "1",
+    photo: require("../../assets/visionSunday.jpg"),
+    title: "Vision Sunday",
+  },
+  {
+    id: "2",
+    photo: require("../../assets/holdOn.jpg"),
+    title: "Hold On",
+  },
+  {
+    id: "3",
+    photo: require("../../assets/findConfidence.jpg"),
+    title: "Find Confidence"
+  }
+];
