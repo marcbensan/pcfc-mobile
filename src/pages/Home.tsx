a50aac10a76db0838a71847448e3e3ba07fc29f9
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-22 22:53:05 -0400

    feat: update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,15 @@
-import { Text } from "react-native-paper";
+import { Divider, Text } from "react-native-paper";
 import CardComponent from "../components/Card";
-import { FlatList, View } from "react-native";
+import { FlatList, ScrollView, View } from "react-native";
 import SubCardComponent from "../components/SubCard";
 import { youtubeData } from "../data/YoutubeData";
+import EventCard from "../components/EventCard";
+import { event } from "../data/EventData";
+import LifeGroups from "../components/LifeGroupsCard";
 
 const Home = () => {
   return (
-    <View style={{ padding: 15 }}>
+    <ScrollView contentContainerStyle={{ padding: 15 }}>
       <CardComponent />
       <Text variant="titleLarge" style={{ marginTop: 35, marginBottom: 5 }}>
         Recently Uploaded
@@ -20,7 +23,22 @@ const Home = () => {
         horizontal
         showsHorizontalScrollIndicator={false}
       />
-    </View>
+      <Divider style={{ marginVertical: 20 }} />
+      <Text variant="titleLarge" style={{ marginBottom: 5 }}>
+        Events
+      </Text>
+      <FlatList
+        data={event}
+        renderItem={({ item }) => (
+          <EventCard title={item.title} description={item.description} />
+        )}
+      />
+      <Divider style={{ marginVertical: 20 }} />
+      <Text variant="titleLarge" style={{ marginBottom: 5 }}>
+        Life Groups
+      </Text>
+      <LifeGroups/>
+    </ScrollView>
   );
 };
 
