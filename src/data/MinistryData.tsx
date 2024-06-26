25a417351c364eb1856efdcb934218fac691ac76
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-25 22:13:33 -0400

    feat: ministry data

---
+++ src/data/MinistryData.tsx
@@ -0,0 +1,17 @@
+export const ministriesData = [
+  {
+    id: "1",
+    title: "Kids",
+    photo: require("../../assets/kids.png"),
+  },
+  {
+    id: "2",
+    title: "Metanoia",
+    photo: require("../../assets/metanoia.jpg"),
+  },
+  {
+    id: "3",
+    title: "Adult",
+    photo: require("../../assets/adult.png"),
+  },
+];
