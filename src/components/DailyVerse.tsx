83332d930552abb4897275c3d2a882ecb13f97e7
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-25 22:13:07 -0400

    feat: daily verse page init

---
+++ src/components/DailyVerse.tsx
@@ -0,0 +1,30 @@
+import { Surface, Text } from "react-native-paper";
+
+const DailyVerse = () => {
+  return (
+    <Surface
+      style={{
+        padding: 30,
+        height: "auto",
+        width: "auto",
+        alignItems: "center",
+      }}
+    >
+      <Text variant="titleMedium"  style={{marginBottom: 20}}>John 3:16</Text>
+      <Text>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam sequi
+        tempore quia fuga, saepe voluptatibus explicabo quas veritatis pariatur
+        placeat possimus optio earum maxime aliquam non eligendi ullam. Tenetur,
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla minus
+        quod debitis eligendi exercitationem nobis, dolor deleniti laboriosam
+        pariatur fugit earum sit temporibus accusantium quae error, dolorem
+        consequatur alias sint. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. At quasi autem, molestias dicta eum id eligendi magnam
+        deleniti! Obcaecati minima eveniet soluta minus magni accusantium
+        repellat nihil. Magnam, nemo deleniti! fugit.
+      </Text>
+    </Surface>
+  );
+};
+
+export default DailyVerse;
