198bb56038c3d9e423f086a9fe57e70be708bed1
Marc <139300806+marcbensan@users.noreply.github.com>
2024-06-26 21:02:01 -0400

    fix: update prayer request page

--- src/pages/PrayerRequest.tsx
+++ src/pages/PrayerRequest.tsx
@@ -1,7 +1,14 @@
 import { Text } from "react-native-paper";
+import AddPostButton from "../components/AddPostButton";
+import Post from "../components/PrayerPosts";
 
 const PrayerRequest = () => {
-  return <Text>Prayer Request</Text>;
+  return (
+    <>
+      <AddPostButton/>
+      <Post/>
+    </>
+  );  
 };
 
 export default PrayerRequest;
